df5302af9cdcc52511b59ff223a3d4caeb0b994c
Sajjad Anwar <me@sajjad.in>
2016-11-05 11:45:54 +0530

    cleanup

--- cow.cpp
+++ cow.cpp
@@ -26,36 +26,34 @@ int main( int argc, char** argv )
     fs::path full_path( fs::initial_path<fs::path>() );
     full_path = fs::system_complete( fs::path( argv[1] ) );
 
+    // read the directory
     fs::directory_iterator end_iter;
     for ( fs::directory_iterator dir_itr( full_path );
           dir_itr != end_iter;
           ++dir_itr )
     {
         std::cout << dir_itr->path().filename() << "\n";
-        // detection
+
+        // use the image and run through the model
         Mat frame = imread(dir_itr->path().string());
         resize(frame,frame,Size(),2,2);
         namedWindow("DPM Cascade Detection", 1);   
         vector<DPMDetector::ObjectDetection> ds;
-        detector->detect(frame, ds); // it somehow eats the image ;(
+        detector->detect(frame, ds);
 
-        t = ((double) getTickCount() - t)/getTickFrequency();//elapsed time
+        t = ((double) getTickCount() - t)/getTickFrequency();
         cerr << "dpm took " << t << " seconds" << endl;
 
+        // look at the score and only write the image if confident.
         for (unsigned int i = 0; i < ds.size(); i++)
         {
             int id = ds[i].classID;
             cerr << names[id] << "\t" << ds[i].score << "\t" << ds[i].rect << endl;
-            //
-            // there's a "<ScoreThreshold>" tag hidden in the xml !
-            //
             if (ds[i].score > -0.90) {
                 rectangle(frame, ds[i].rect, Scalar(255, 255, 255), 1);
                 imwrite(dir_itr->path().filename().string(), frame);
             }
         }
-
-        // imshow("DPM Cascade Detection", frame);
     }
 
     return 0;
